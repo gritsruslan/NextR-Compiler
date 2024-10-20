14494d3c4596338fe04bdb1ee1adbec49c4f6195
gritsruslan <gritsruslan@gmail.com>
2024-10-21 02:54:57 +0300

    Add removing single line comments impl

--- src/NextR-Compiler/Preprocessor/Preprocessor.cs
+++ src/NextR-Compiler/Preprocessor/Preprocessor.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace NextR_Compiler.Preprocessor;
 
 #if DEBUG
@@ -7,10 +9,12 @@ namespace NextR_Compiler.Preprocessor;
 #endif
 class Preprocessor(string sourceCode)
 {
-
 	public string GetPreprocessedCode()
 	{
-		return sourceCode;
+		string preprocessedCode = RemoveSingleLineComments(sourceCode);
+		preprocessedCode = RemoveMultiLineComments(preprocessedCode);
+
+		return preprocessedCode;
 	}
 
 	private char GetChar(int position) => position < sourceCode.Length ? sourceCode[position] : '\0';
@@ -18,7 +22,43 @@ class Preprocessor(string sourceCode)
 	//TODO
 	private string RemoveSingleLineComments(string code)
 	{
-		throw new NotImplementedException();
+		const char slash = '/';
+		const char endOfLine = '\n';
+		const char doubleQuote = '"';
+
+		var codeSb = new StringBuilder();
+
+		bool isInStringLiteral = false;
+		bool isInSingleLineComment = false;
+
+		for (int pos = 0; pos < code.Length; pos++)
+		{
+			var current = GetChar(pos);
+			var next = GetChar(pos + 1);
+
+			if (current == doubleQuote && !isInSingleLineComment &&
+			    (pos == 0 || GetChar(pos - 1) != '\\')) // skip if escape character
+			{
+				isInStringLiteral = !isInStringLiteral;
+			}
+
+			if (current == slash && next == slash && !isInStringLiteral)
+			{
+				isInSingleLineComment = true;
+			}
+
+			if (current == endOfLine)
+			{
+				isInSingleLineComment = false;
+			}
+
+			if (!isInSingleLineComment)
+			{
+				codeSb.Append(current);
+			}
+		}
+
+		return codeSb.ToString();
 	}
 
 	//TODO
