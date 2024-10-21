f0f6ef46e8dd0273f5cb5c8f7538584caecf37e3
gritsruslan <gritsruslan@gmail.com>
2024-10-21 12:55:17 +0300

    Add TokenType.cs

---
+++ src/NextR-Compiler/Tokens/TokenType.cs
@@ -0,0 +1,76 @@
+namespace NextR_Compiler.Tokens;
+
+public enum TokenType
+{
+	//Operations
+	Plus,
+	Minus,
+	Multiply,
+	Divide,
+	PlusEquals,
+	MinusEquals,
+	MultiplyEquals,
+	DivideEquals,
+	RemainderDiv,
+	RemainderDivEquals,
+	Equals,
+
+	//Bool Operations
+	Less,
+	LessOrEqual,
+	Greater,
+	GreaterOrEqual,
+	BoolEquals,
+	BoolNoEquals,
+	BoolNo,
+	And,
+	Or,
+	Is,
+
+	//Values
+	IntLiteral,
+	UintLiteral,
+	FloatLiteral,
+	StringLiteral,
+	CharLiteral,
+
+	//TypeKeywords
+	IntKeyword,
+	UintKeyword,
+	FloatKeyword,
+	StringKeyword,
+	CharKeyword,
+
+	//Keywords
+	FuncKeyword,
+	ForKeyword,
+	WhileKeyword,
+	LoopKeyword,
+	VarKeyword,
+	BreakKeyword,
+	ContinueKeyword,
+	ConstKeyword,
+	IfKeyword,
+	ElseKeyword,
+	TrueKeyword,
+	FalseKeyword,
+	CastKeyword,
+
+	//Symbols
+	Semicolon,
+	OpenParenthesis,
+	CloseParenthesis,
+	OpenSquareBracket,
+	CloseSquareBracket,
+	OpenCurlyBracket,
+	CloseCurlyBracket,
+	DoubleQuote,
+	SingleQuote,
+	Comma,
+	Dot,
+	Colon,
+
+	// Other
+	Identifier,
+	EndOfFile,
+}
